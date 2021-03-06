69f05e9d845a829ba509bd52dc676e51f7fff5b0
0801euiyeon <qq212420@gmail.com>
2021-03-06 22:06:13 +0900

    전체 CSS 수정

--- crud/forms.py
+++ crud/forms.py
@@ -4,15 +4,30 @@ from .models import Crud, Info,  Qna
 class CrudUpdate(forms.ModelForm):
     class Meta:
         model = Crud
-        fields = ['title', 'body', 'name']
+        fields = ['title', 'name', 'body']
+        lables = {
+        'title':'제목' ,
+        'name': '작성자', 
+        'body' : '본문'
+        }
 
 class InfoUpdate(forms.ModelForm):
     class Meta:
         model = Info
-        fields = ['title', 'body', 'name']
+        fields = ['title', 'name', 'body']
+        lables = {
+        'title':'제목' ,
+        'name': '작성자', 
+        'body' : '본문'
+        }
 
 class QnaUpdate(forms.ModelForm):
     class Meta:
         model = Qna
-        fields = ['title', 'body', 'name']
+        fields = ['title', 'name','body']
+        lables = {
+        'title':'제목' ,
+        'name': '작성자', 
+        'body' : '본문'
+        }
 
